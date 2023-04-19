544e8f3445a4354a648c29dd8452cddf15aa924a
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-04-19 15:17:42 +0200

    feat(core): Add public api for transferring credentials/workflow ownership

--- packages/cli/src/PublicApi/v1/handlers/users/users.service.ts
+++ packages/cli/src/PublicApi/v1/handlers/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Container } from 'typedi';
-import { RoleRepository } from '@db/repositories';
+import { RoleRepository, UserRepository } from '@db/repositories';
 import type { Role } from '@db/entities/Role';
 import type { User } from '@db/entities/User';
 
@@ -9,4 +9,10 @@ export function isInstanceOwner(user: User): boolean {
 
 export async function getWorkflowOwnerRole(): Promise<Role> {
 	return Container.get(RoleRepository).findWorkflowOwnerRoleOrFail();
+}
+
+export async function getUserIdByEmail(email: string): Promise<User['id'] | undefined> {
+	return Container.get(UserRepository)
+		.findOne({ select: ['id'], where: { email } })
+		.then((u) => u?.id);
 }
